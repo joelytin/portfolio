0811102825c665b001472108452f53bcb1625466
joelytin <joelytin@outlook.com>
2024-10-24 16:56:57 +0800

    Added some comments

--- src/utils/Themes.js
+++ src/utils/Themes.js
@@ -1,11 +1,11 @@
 export const darkTheme = {
     bg:"#1C1C27",
     bgLight: "#1C1E27",
-    primary:"#854CE6",
-    text_primary:"#F2F3F4",
-    text_secondary:"#b1b2b3",
-    card:"#171721",
-    card_light: '#191924',
+    primary:"#854CE6", // role
+    text_primary:"#F2F3F4", // white
+    text_secondary:"#b1b2b3", // light grey
+    card:"#171721", // card background
+    card_light: '#191924', // header background
     button:"#854CE6",
     white:"#FFFFFF",
     black:"#000000",
